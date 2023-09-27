8c0026eebe14091f97eb6edfd9c1c586566374e1
anish <anishyadav7853@gmail.com>
2023-09-28 01:06:15 +0530

    commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.post("/contact", (req, res) => {
         requireTLS: true,
         auth: {
           user: "anishyadav7853@gmail.com",
-          pass: "ceaqjzkdrecljnde",
+          pass: process.env.PASSWORD
         },
       });
       var success = false;
